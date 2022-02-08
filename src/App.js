3c4dd83540d0f921c29fedc0aac3e634e67c377e
djjean <djjean90@gmail.com>
2022-02-08 09:28:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react/cjs/react.production.min';
+import Header from './components/Layout/Header';
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
