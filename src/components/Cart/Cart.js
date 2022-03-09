890c55a5a85c0639414b64666d4ce2a667facc4b
MM work <djjean90@gmail.com>
2022-03-09 19:39:11 +0200

    home

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,11 +10,22 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItem = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => {
         return (
-          <CartItem name={item.name} price={item.price} amount={item.amount} />
+          <CartItem
+            key={item.id}
+            name={item.name}
+            price={item.price}
+            amount={item.amount}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
         );
       })}
     </ul>
